190d91f863e8e732b7bede667e68a7ed95fefd7c
redoti <49931988+redoti@users.noreply.github.com>
2023-11-07 11:12:59 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.static_folder = 'static'
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/profile-bootstrap.html")
+def profile():
+    return render_template("profile-bootstrap.html")
+
+@app.route("/form-bootstrap.html")
+def form():
+    return render_template("form-bootstrap.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
